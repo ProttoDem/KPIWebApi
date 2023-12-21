c5e967957b684456719611e7d48027a2b8f3eb78
dmitrii nieliepin <dmitrii.nieliepin@formpipe.com>
2023-12-22 01:01:17 +0200

    Added basic repositories and unitOfWork

---
+++ TaskWebApiLab/TaskWebApiLab/UnitOfWork/CategoryRepository.cs
@@ -0,0 +1,22 @@
+﻿using Data.Models;
+
+namespace TaskWebApiLab.UnitOfWork
+{
+    public class CategoryRepository : IRepository<Category>
+    {
+        public Category GetById(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList<Category> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Category entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
